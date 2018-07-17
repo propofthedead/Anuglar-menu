925b80ba69fe53d0a36f0f127c6d11b2177aa1e0
Foster Stule <camodude5@yahoo.com>
2018-07-17 11:16:21 -0400

    finally got up

--- src/app/menu-item/menu-item.component.ts
+++ src/app/menu-item/menu-item.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-menu-item',
+  selector: 'menu-item',
   templateUrl: './menu-item.component.html',
   styleUrls: ['./menu-item.component.css']
 })
 export class MenuItemComponent implements OnInit {
-
+  @Input() menu;
   constructor() { }
 
   ngOnInit() {
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
+  menudata=[
+    {href: '/home', display: 'Home'},
+    {href:'/about',display:'About'},
+    {href: '/help',display:'Help'},
+    {href:'/contact', display: 'Contact'}];
   constructor() { }
+
 
   ngOnInit() {
   }
